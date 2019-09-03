5851571ea968a6a038cb83d6950f166cb42533b9
Kamil T <trzaskomakamil@gmail.com>
2019-09-03 22:14:32 +0200

    random color generator added

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-let colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)'
-]
+let colors = generateRandomColors(6)
 
 let squares = document.querySelectorAll('.square')
 let pickedColor = pickColor()
@@ -23,6 +16,7 @@ for (let i = 0; i < squares.length; i++) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = 'Correct!'
       changeColors(clickedColor)
+      document.querySelector('h1').style.background = pickedColor
     } else {
       this.style.background = '#232323'
       messageDisplay.textContent = 'Try Again'
@@ -34,10 +28,26 @@ function changeColors(color) {
   for (let i = 0; i < colors.length; i++) {
     squares[i].style.background = color
   }
-  document.querySelector('h1').style.background = color
 }
 
 function pickColor() {
   let random = Math.floor(Math.random() * colors.length)
   return colors[random]
+}
+
+function generateRandomColors(num) {
+  let arr = []
+
+  for (let i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+
+  return arr
+}
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256)
+  let g = Math.floor(Math.random() * 256)
+  let b = Math.floor(Math.random() * 256)
+  return `rgb(${r}, ${g}, ${b})`
 }
