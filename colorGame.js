8828ecf9385d6271472d83cbe0e2ae18425805af
Kamil T <trzaskomakamil@gmail.com>
2019-09-03 22:49:46 +0200

    full functionality, before styling and refactoring

--- colorGame.js
+++ colorGame.js
@@ -4,6 +4,51 @@ let squares = document.querySelectorAll('.square')
 let pickedColor = pickColor()
 let colorDisplay = document.getElementById('colorDisplay')
 let messageDisplay = document.getElementById('message')
+let h1 = document.querySelector('h1')
+let resetButton = document.querySelector('#reset')
+let easyBtn = document.querySelector('#easyBtn')
+let hardBtn = document.querySelector('#hardBtn')
+
+easyBtn.addEventListener('click', function() {
+  easyBtn.classList.add('selected')
+  hardBtn.classList.remove('selected')
+  numSquares = 3
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.background = colors[i]
+    } else {
+      squares[i].style.display = 'none'
+    }
+  }
+})
+
+hardBtn.addEventListener('click', function() {
+  hardBtn.classList.add('selected')
+  easyBtn.classList.remove('selected')
+  numSquares = 6
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i]
+    squares[i].style.display = 'block'
+  }
+})
+
+resetButton.addEventListener('click', function() {
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i]
+  }
+
+  h1.style.background = '#232323'
+})
 
 colorDisplay.textContent = pickedColor
 
@@ -15,8 +60,9 @@ for (let i = 0; i < squares.length; i++) {
 
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = 'Correct!'
+      resetButton.textContent = 'Play Again?'
       changeColors(clickedColor)
-      document.querySelector('h1').style.background = pickedColor
+      h1.style.background = pickedColor
     } else {
       this.style.background = '#232323'
       messageDisplay.textContent = 'Try Again'
